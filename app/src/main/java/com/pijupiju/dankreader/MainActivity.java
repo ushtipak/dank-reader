8a25bfe347e6e37c1ae04bce411a562dbaf3cf60
Milos Knezevic <ushtipak@gmail.com>
2019-11-20 21:55:07 +0100

    Add Neanderthal-like input read into textView :)

--- app/src/main/java/com/pijupiju/dankreader/MainActivity.java
+++ app/src/main/java/com/pijupiju/dankreader/MainActivity.java
@@ -1,9 +1,15 @@
 package com.pijupiju.dankreader;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        initViews();
+    }
+
+    private void initViews() {
+        String s = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        InputStream inputStream = this.getResources().openRawResource(R.raw.input);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        while (true) {
+            try {
+                if ((s = bufferedReader.readLine()) == null) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            stringBuilder.append(s).append("\n");
+        }
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(stringBuilder);
     }
 }
